fa9cb31117c2e384090cc74d99e3e60366992a98
simpukr <utkarsh.email@yahoo.com>
2018-09-28 23:45:04 +0530

    Networking

--- app/src/main/java/com/theimprovisers/iitrconnect/NetworkMethods.java
+++ app/src/main/java/com/theimprovisers/iitrconnect/NetworkMethods.java
@@ -1,5 +1,18 @@
 package com.theimprovisers.iitrconnect;
 
+import android.util.Log;
+
+import com.google.firebase.firestore.FirebaseFirestore;
+
 public class NetworkMethods
 {
+    private static FirebaseFirestore database;
+    private static boolean initialised = false;
+
+    public static void Initialise(FirebaseFirestore database)
+    {
+        NetworkMethods.database = database;
+        initialised = true;
+        Log.d("NetworkMethods","Database initialised");
+    }
 }
