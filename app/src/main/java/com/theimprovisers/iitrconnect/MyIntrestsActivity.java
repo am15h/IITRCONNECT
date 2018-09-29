939bd650d962f765763d2e6ec01275e24ddaeb01
ManasviPatidar <mpatidar@cs.iitr.ac.in>
2018-09-29 17:29:19 +0530

    pushing res

--- app/src/main/java/com/theimprovisers/iitrconnect/MyIntrestsActivity.java
+++ app/src/main/java/com/theimprovisers/iitrconnect/MyIntrestsActivity.java
@@ -1,33 +1,122 @@
 package com.theimprovisers.iitrconnect;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class MyIntrestsActivity extends AppCompatActivity {
 private Button buttonProgrammingLanguage;
-private Button buttonCulturalIntrest;
-private Button buttonSportsIntrest;
+private Button buttonCulturalInterest;
+private Button buttonSportsInterest;
 private Button buttonClubs;
+private String[] listItems;
+private String[] listItemsProgrammingLanguage;
+private String[] listItemsCulturalInterest;
+private String[] listItemsSportsInterest;
+private String[] listItemsClubs;
+
+private boolean[] checkedItems;
+private boolean[] checkedItemsProrgrammingLanguage;
+private boolean[] checkedItemsSportsInterest;
+private boolean[] checkedItemsCulturalInterest;
+private boolean[] checkedItemsClubs;
+
+private ArrayList<Integer>userItems = new ArrayList<>();
+private ArrayList<Integer>userItemsProgrammingLanguage = new ArrayList<>();
+private ArrayList<Integer>userItemsCulturalInterest = new ArrayList<>();
+private ArrayList<Integer>userItemsSportsInterest = new ArrayList<>();
+private ArrayList<Integer>userItemsClubs = new ArrayList<>();
+
+    Profile mProfile = new Profile();
+    Tag mTag;
+    String[] tagsArray  = new String[mProfile.tags.length];
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_intrests);
 
         buttonProgrammingLanguage = (Button) findViewById(R.id.button_ProgrammingLaguage);
-        buttonCulturalIntrest =(Button) findViewById(R.id.button_CulturalIntrest);
-        buttonSportsIntrest = (Button) findViewById(R.id.button_SportsIntrest);
-        buttonClubs = (Button) findViewById(R.id.button_Clubs);
+
+        for(int i = 0; i < mProfile.tags.length; i++) {
+            tagsArray[i] = mProfile.tags[i].name;
+        }
+        //list
+        listItemsProgrammingLanguage = Arrays.copyOfRange(tagsArray,0,5);
+        checkedItemsProrgrammingLanguage = new boolean[listItemsProgrammingLanguage.length];
 
         buttonProgrammingLanguage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                openDialog();
+                AlertDialog.Builder mBuilder = new AlertDialog.Builder(MyIntrestsActivity.this);
+                mBuilder.setTitle("Select Programming Language");
+                mBuilder.setMultiChoiceItems(listItemsProgrammingLanguage, checkedItemsProrgrammingLanguage, new DialogInterface.OnMultiChoiceClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int position, boolean isChecked) {
+                        //Operation here
+                        if(isChecked){
+                            //here i m adding in array list
+                            if (!userItemsProgrammingLanguage.contains(position)){
+                                userItemsProgrammingLanguage.add(position);
+                            }else{
+                                userItemsProgrammingLanguage.remove(position);
+                            }
+                        }
+                    }
+                });
+                mBuilder.setCancelable(false);
+                mBuilder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        String item = "";
+                        for(int i = 0;i<userItemsProgrammingLanguage.size();i++){
+                            //listItemsProgrammingLanguage[userItems.get(i)] gives the tags
+
+                            //Code for Uploading goes here.
+
+                        }
+                    }
+                });
+                mBuilder.setNegativeButton("Dismiss", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+                mBuilder.setNeutralButton("Clear all", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        for(int i =0;i<checkedItemsProrgrammingLanguage.length; i++ ){
+                        checkedItemsProrgrammingLanguage[i] = false;
+                        userItemsProgrammingLanguage.clear();
+                        }
+                    }
+                });
+                AlertDialog mDialog = mBuilder.create();
+                mDialog.show();
+
             }
         });
+
+        buttonCulturalInterest =(Button) findViewById(R.id.button_CulturalIntrest);
+
+
+
+        buttonSportsInterest = (Button) findViewById(R.id.button_SportsIntrest);
+
+
+
+        buttonClubs = (Button) findViewById(R.id.button_Clubs);
+
+
+
     }
 
-    private void openDialog() {
-    }
+
 }
--- app/src/main/java/com/theimprovisers/iitrconnect/Profile.java
+++ app/src/main/java/com/theimprovisers/iitrconnect/Profile.java
@@ -42,12 +42,28 @@ public class Profile
         this.email = "";
         this.year = 0;
 
-        tags = new Tag[5];
+        tags = new Tag[20];
         tags[0] = new Tag("tag0");
         tags[1] = new Tag("tag1");
         tags[2] = new Tag("tag2");
         tags[3] = new Tag("tag3");
         tags[4] = new Tag("tag4");
+        tags[5] = new Tag("tag0");
+        tags[6] = new Tag("tag1");
+        tags[7] = new Tag("tag2");
+        tags[8] = new Tag("tag3");
+        tags[9] = new Tag("tag4");
+        tags[10] = new Tag("tag1");
+        tags[11] = new Tag("tag2");
+        tags[12] = new Tag("tag3");
+        tags[13] = new Tag("tag4");
+        tags[14] = new Tag("tag0");
+        tags[15] = new Tag("tag1");
+        tags[16] = new Tag("tag2");
+        tags[17] = new Tag("tag3");
+        tags[18] = new Tag("tag4");
+        tags[19] = new Tag("tag0");
+
     }
 
     public Map<String,Object> GetMap()
