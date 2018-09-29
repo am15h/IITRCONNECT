468e38fc021f507b07fa50e80a746fbaccd7289b
simpukr <utkarsh.email@yahoo.com>
2018-09-29 09:51:24 +0530

    Networking

--- app/src/main/java/com/theimprovisers/iitrconnect/Tag.java
+++ app/src/main/java/com/theimprovisers/iitrconnect/Tag.java
@@ -4,6 +4,7 @@ public class Tag
 {
     public String name;
     public boolean value;
+    public static Tag[] predefinedTags = {new Tag("tag0"),new Tag("tag1"),new Tag("tag2"),new Tag("tag3"),new Tag("tag4")};
 
     public Tag(String name,boolean value)
     {
